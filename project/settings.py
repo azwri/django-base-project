84ad78f215d0c497fcb7d9a783696a7c2eb1ce0c
aazwri <aazwri@gmail.com>
2022-01-25 13:52:17 +0300

    pip install mysqlclient & add mysql db to localsetting

--- project/settings.py
+++ project/settings.py
@@ -77,13 +77,15 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+# add mysql db
+from .local_settings import DATABASES
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
